0de1296932a98ee0d3c7622d3f227f316c52e5f0
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2020-04-02 22:37:47 +0600

    created a class in update model

--- restAPI/settings.py
+++ restAPI/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'updates',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- updates/models.py
+++ updates/models.py
@@ -1,8 +1,19 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
+
 # Create your models here.
 
 
-class Updates(models.Model):
-    User            = models.ForeignKey(settings.AUTH_USER_MODEL)
-    
+def upload_update_image(instance, filename):
+    return "updates/{user}/{filename}".format(user=instance.user,filename=filename)
+
+
+class Update(models.Model):
+    User            = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.DO_NOTHING,)
+    content         = models.TextField()
+    image           = models.ImageField(upload_to=upload_update_image,blank=True,null=True)
+    timestamp       = models.DateTimeField(auto_now_add=True)
+    updated         = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content or ""
