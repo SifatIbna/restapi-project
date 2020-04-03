2d59d2dfe636251912b71bd26c2b1a9f39dfebb7
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2020-04-03 20:26:16 +0600

    created api folder to separate methods

---
+++ updates/api/views.py
@@ -0,0 +1,19 @@
+from updates.model import Updates as UpdateModel
+
+
+class UpdateModelAPI():
+    def get(self, request, *args, **kwargs):
+        return #json
+
+    def post(self, request, *args, **kwargs):
+        return #json
+
+    def put(self, request, *args, **kwargs):
+        return #json
+
+    def delete(self, request, *args, **kwargs):
+        return #json
+
+class UpdateModelListAPIView():
+    def get(self, request, *args, **kwargs):
+        return #json
