8d2ed5ea609e0de2667b7c4f0560cd3fb1aa1774
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2020-04-05 14:55:41 +0600

    trying to see how api endpoints work through script and a mixins in api folder to handle CSRF

--- scripts/rest_pure.api.py
+++ scripts/rest_pure.api.py
@@ -1,4 +1,5 @@
 import requests
+import json 
 
 BASE_URL = " http://127.0.0.1:8000/"
 
@@ -7,9 +8,24 @@ ENDPOINT = "api/updates/"
 def get_list():
     r = requests.get(BASE_URL + ENDPOINT)
     data = r.json()
+    # print(data)
     for obj in data:
-        print(obj['pk'])
-    return data
+        r2 = requests.get(BASE_URL + ENDPOINT + str(obj['pk']))
+        # print(r2.json())
+        return data
 
+def create_update():
+    data = {
+        'user':1,
+        "content":"Another new cool update",
+    }
+    r = requests.delete(BASE_URL+ENDPOINT,data=data)
 
-print(get_list())
+    print(r.headers)
+    print(r.status_code)
+    if r.status_code == requests.codes.ok:
+        #print(r.json)
+        return r.json()
+    return r.text
+
+print(create_update())
--- updates/api/views.py
+++ updates/api/views.py
@@ -1,9 +1,10 @@
+import json
 from django.views.generic import View
 from django.http import HttpResponse
 from updates.models import Updates as UpdateModel
+from .mixins import CSRFExemptMixin
 
-
-class UpdateModelDetailAPIView(View):
+class UpdateModelDetailAPIView(CSRFExemptMixin,View):
     def get(self, request,id ,*args, **kwargs):
         obj = UpdateModel.objects.get(id=id)
         json_data = obj.serialize()
@@ -20,8 +21,18 @@ class UpdateModelDetailAPIView(View):
     def delete(self, request, *args, **kwargs):
         return HttpResponse({},content_type='application/json')
 
-class UpdateModelListAPIView(View):
+class UpdateModelListAPIView(CSRFExemptMixin,View):
     def get(self, request, *args, **kwargs):
         qs = UpdateModel.objects.all()
         json_data = qs.serialize()
         return HttpResponse(json_data,content_type='application/json')
+
+    def post(self,request,*args, **kwargs):
+        
+        data = json.dumps({'content' : 'Nothing is real'})
+        return HttpResponse(data,content_type='application/json')
+
+    def delete(self,request,*args, **kwargs):
+        
+        data = json.dumps({'content' : 'You can not delte an entire list'})
+        return HttpResponse(data,content_type='application/json')
